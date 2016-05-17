042145588c617ed74deaef3d8d7b87e03eb12b06
mdlawrenceusa <mdlawrence@me.com>
2016-05-17 12:03:05 -0400

    Initial Commmit

---
+++ js/_vti_cnf/foundation.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|16 Feb 2016 15:57:13 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|ET\\Instructor
+vti_modifiedby:SR|ET\\Instructor
+vti_timecreated:TR|16 Feb 2016 15:57:13 -0000
+vti_cacheddtm:TX|16 Feb 2016 15:57:13 -0000
+vti_filesize:IR|248310
+vti_backlinkinfo:VX|
